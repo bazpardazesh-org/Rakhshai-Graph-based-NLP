cd8a9c3007a0c538771fbf941741a3e4247b664f
Rakhshai Dev Group <info@bazpardazesh.com>
2025-11-16 20:53:26 +0330

    Update test_gat_speed_equiv.py

--- tests/test_gat_speed_equiv.py
+++ tests/test_gat_speed_equiv.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+from rakhshai_graph_nlp.features.pyg_data import graph_to_data
 from rakhshai_graph_nlp.graphs.graph import Graph
-from rakhshai_graph_nlp.models.gat import GATClassifier
-
+from rakhshai_graph_nlp.tasks.classification import train_node_classifier
 
 def test_gat_runs():
     g = Graph(nodes=[0, 1], adjacency=np.ones((2, 2)))
     X = np.eye(2)
-    model = GATClassifier(input_dim=2, hidden_dim=2, num_classes=2)
-    model.fit(g, X, np.array([0, 1]), num_epochs=1)
-    preds = model.predict(g, X)
+    labels = np.array([0, 1])
+    model, _ = train_node_classifier(g, labels, X=X, model_type="gat", num_epochs=1)
+    data = graph_to_data(g, features=X, labels=labels)
+    preds = model.predict(data)
     assert preds.shape == (2,)
